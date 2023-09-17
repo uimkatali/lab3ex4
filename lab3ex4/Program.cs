30f9cd054a2e850d1dafac13e50d4cf60c55e075
uimkatali <b8bf6b5@gmail.com>
2023-09-17 21:10:42 +0300

    Add project files.

---
+++ lab3ex4/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace lab1ex1
+{
+    /*
+     Scrieti un program care va afisa semnul unui numar citit de la tastatura
+    • Daca numarul este pozitiv, va afisa “+”
+    • Daca numarul este negativ, va afisa “-”
+    • Daca numarul este 0, va afisa “0”
+     */
+    class Program
+    {
+        private const string Value = "-";
+
+        static void Main(string[] args)
+        {
+            double numar = Convert.ToDouble(Console.ReadLine());  
+            if(numar == 0)
+            {
+                Console.WriteLine("0");
+            }
+            if (numar > 0) {
+                Console.WriteLine("+");
+            }if (numar < 0) {
+                Console.WriteLine("-");
+            }
+        }
+    }
+}
